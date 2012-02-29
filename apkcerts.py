4fc0dae2394bd177de3ed02348917a79d0c3fb85
yumingyang <yumingyang@xiaomi.com>
2012-02-29 19:25:28 +0800

    modify apkcerts.py to create apkcert txt
    Change-Id: I8a8749139c9942e49b6d9394d519124f0f57adba

--- apkcerts.py
+++ apkcerts.py
@@ -6,19 +6,17 @@ import xml
 import xml.dom
 from xml.dom import minidom
 
-def insertToDestText(fileContent, uid, name, shareUserId):
-    if -1 == string.rfind(fileContent, name):
-        if (1000 == uid) and (1001 == uid):
-            node = "name=\"" + name + "\" certificate=\"build/target/product/security/platform.x509.pem\" private_key=\"build/target/product/security/platform.pk8\""
-        elif 1013 == uid:
-            node = "name=\"" + name + "\" certificate=\"build/target/product/security/media.x509.pem\" private_key=\"build/target/product/security/media.pk8\""
-        elif shareUserId == uid:
-            node = "name=\"" + name + "\" certificate=\"build/target/product/security/shared.x509.pem\" private_key=\"build/target/product/security/shared.pk8\""
-        else:
-            node = "name=\"" + name + "\" certificate=\"build/target/product/security/testkey.x509.pem\" private_key=\"build/target/product/security/testkey.pk8\""
-        print "Insert --> uid: %d     node: %s" %(uid, node)
-        return  node + "\n"
-    return ""
+def insertToDestText(fileContent, uid, name, shareUserId):    
+    if (1000 == uid) or (1001 == uid):
+        node = "name=\"" + name + "\" certificate=\"build/target/product/security/platform.x509.pem\" private_key=\"build/target/product/security/platform.pk8\""
+    elif 1013 == uid:
+        node = "name=\"" + name + "\" certificate=\"build/target/product/security/media.x509.pem\" private_key=\"build/target/product/security/media.pk8\""
+    elif shareUserId == uid:
+        node = "name=\"" + name + "\" certificate=\"build/target/product/security/shared.x509.pem\" private_key=\"build/target/product/security/shared.pk8\""
+    else:
+        node = "name=\"" + name + "\" certificate=\"build/target/product/security/testkey.x509.pem\" private_key=\"build/target/product/security/testkey.pk8\""
+    print "Insert --> uid: %d     node: %s" %(uid, node)
+    return  node + "\n"
 
 def getName(codePath):
     if string.rfind(codePath, "/system/app/") > -1:
@@ -27,40 +25,35 @@ def getName(codePath):
         return ""
 
 def usage():
-    print "Usage: python ./apkcerts_merge.py src dest"
+    print "Usage: python ./apkcerts.py path-to-packages.xml path-to-apkcerts.txt"
 
-if len(sys.argv) != 3:
-    usage()
-    sys.exit(1)
+def main():
+    if len(sys.argv) != 3:
+        usage()
+        sys.exit(1)
 
-try:
-    file = open(sys.argv[2], "r")
-except IOError:
-    print "Error: %s doesn't exist" %(sys.argv[2])
-    sys.exit(1)
+    try:
+        xmldoc = minidom.parse(sys.argv[1])
+        packages = xmldoc.getElementsByTagName("package")
+    except :
+        print "Error: %s doesn't exist or isn't a vaild xml file" %(sys.argv[1])
+        sys.exit(1)
 
-try:
-    xmldoc = minidom.parse(sys.argv[1])
-    packages = xmldoc.getElementsByTagName("package")
-except :
-    print "Error: %s doesn't exist or isn't a vaild xml file" %(sys.argv[1])
-    sys.exit(1)
-
-for pkg in packages:
-    if pkg.attributes["codePath"].value == "/system/app/ContactsProvider.apk":
-        shareUserId = string.atoi(pkg.attributes["sharedUserId"].value)
-        #print shareUserId
-        break
-
-fileContent = file.read()
-file.close()
-for pkg in packages:
-    if pkg.hasAttribute("userId"):
-            uid = string.atoi(pkg.attributes["userId"].value)
-    else:
-            uid = string.atoi(pkg.attributes["sharedUserId"].value)
-    name = getName(pkg.attributes["codePath"].value)
-    if name != "" :
+    for pkg in packages:
+        if pkg.attributes["codePath"].value == "/system/app/ContactsProvider.apk":
+            shareUserId = string.atoi(pkg.attributes["sharedUserId"].value)
+            #print shareUserId
+            break
+        
+    fileContent = ""
+    for pkg in packages:
+        if pkg.hasAttribute("userId"):
+                uid = string.atoi(pkg.attributes["userId"].value)
+        else:
+                uid = string.atoi(pkg.attributes["sharedUserId"].value)
+        name = getName(pkg.attributes["codePath"].value)
         fileContent +=  insertToDestText(fileContent, uid, name, shareUserId)
 
-open(sys.argv[2], "w").write(fileContent)
+    open(sys.argv[2], "w").write(fileContent)
+if "__main__" == __name__:
+    main()
