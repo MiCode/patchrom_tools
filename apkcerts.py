07fa3f63e522e46092de6dbe60a430217f506dde
yumingyang <yumingyang@xiaomi.com>
2012-02-29 11:52:58 +0800

    a tool extract apkcerts from src xml file and insert them to dest  apkcerts txt file
    Change-Id: I4af1cc31e7e08aa057760555fdf5f29de6ab9ac1

---
+++ apkcerts.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import sys
+import string
+import xml
+import xml.dom
+from xml.dom import minidom
+
+def insertToDestText(fileContent, uid, name, shareUserId):
+    if -1 == string.rfind(fileContent, name):
+        if (1000 == uid) and (1001 == uid):
+            node = "name=\"" + name + "\" certificate=\"build/target/product/security/platform.x509.pem\" private_key=\"build/target/product/security/platform.pk8\""
+        elif 1013 == uid:
+            node = "name=\"" + name + "\" certificate=\"build/target/product/security/media.x509.pem\" private_key=\"build/target/product/security/media.pk8\""
+        elif shareUserId == uid:
+            node = "name=\"" + name + "\" certificate=\"build/target/product/security/shared.x509.pem\" private_key=\"build/target/product/security/shared.pk8\""
+        else:
+            node = "name=\"" + name + "\" certificate=\"build/target/product/security/testkey.x509.pem\" private_key=\"build/target/product/security/testkey.pk8\""
+        print "Insert --> uid: %d     node: %s" %(uid, node)
+        return  node + "\n"
+    return ""
+
+def getName(codePath):
+    if string.rfind(codePath, "/system/app/") > -1:
+        return string.replace(codePath, "/system/app/", "")
+    else :
+        return ""
+
+def usage():
+    print "Usage: python ./apkcerts_merge.py src dest"
+
+if len(sys.argv) != 3:
+    usage()
+    sys.exit(1)
+
+try:
+    file = open(sys.argv[2], "r")
+except IOError:
+    print "Error: %s doesn't exist" %(sys.argv[2])
+    sys.exit(1)
+
+try:
+    xmldoc = minidom.parse(sys.argv[1])
+    packages = xmldoc.getElementsByTagName("package")
+except :
+    print "Error: %s doesn't exist or isn't a vaild xml file" %(sys.argv[1])
+    sys.exit(1)
+
+for pkg in packages:
+    if pkg.attributes["codePath"].value == "/system/app/ContactsProvider.apk":
+        shareUserId = string.atoi(pkg.attributes["sharedUserId"].value)
+        #print shareUserId
+        break
+
+fileContent = file.read()
+file.close()
+for pkg in packages:
+    if pkg.hasAttribute("userId"):
+            uid = string.atoi(pkg.attributes["userId"].value)
+    else:
+            uid = string.atoi(pkg.attributes["sharedUserId"].value)
+    name = getName(pkg.attributes["codePath"].value)
+    if name != "" :
+        fileContent +=  insertToDestText(fileContent, uid, name, shareUserId)
+
+open(sys.argv[2], "w").write(fileContent)
