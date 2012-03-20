169ac142c009a147c751bfe51ce8072c6bf04676
wangwenjun <wangwenjun@xiaomi.com>
2012-03-20 16:57:12 +0800

    support use_data_fonts
    Change-Id: I52213b297818e78974b6620747fa93f03f0d95ca

--- set_build_prop.sh
+++ set_build_prop.sh
@@ -7,5 +7,6 @@ cat $build_prop_file | sed -e "s/ro\.build\.version\.incremental=.*/ro\.build\.v
                       | sed -e "s/ro\.product\.mod_device=.*//" > $build_prop_file.new
 
 echo "ro.product.mod_device=$2" >> $build_prop_file.new
+echo "ro.skia.use_data_fonts=1" >> $build_prop_file.new
 mv $build_prop_file.new $build_prop_file
 
