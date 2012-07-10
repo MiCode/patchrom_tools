45032efa16d2b0968cafea7b5081e049c2a517a3
gexudong <gexudong@xiaomi.com>
2012-07-10 14:15:22 +0800

    use MiuiHome/res/xml/default_workspace.xml.part to overlay the desktop layout
    Change-Id: Idc0756b6758687b4f6161e4d0c0e47c1fba056e4

--- patch_miui_app.sh
+++ patch_miui_app.sh
@@ -6,3 +6,12 @@
 if [ -f "customize_miui_app.sh" ]; then
 	./customize_miui_app.sh $1 $2
 fi
+
+if [ $1 = "MiuiHome" ];then
+   if [ -f $1/res/xml/default_workspace.xml.part ]; then
+       $PORT_ROOT/tools/gen_desklayout.pl $1/res/xml/default_workspace.xml.part $2/res/xml
+       for file in $2/res/xml/default_workspace*.xml; do
+           mv $file.new $file
+       done
+   fi
+fi
