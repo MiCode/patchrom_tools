c0740e3fa20ad814a04234f23112aed38646fe1c
mochangming <mochangming@xiaomi.com>
2013-03-26 18:44:11 +0800

    update fix_9patch_png.sh
    Change-Id: I645fdb1eafa8e06052d1a20dd0abf5a5252b5324

--- fix_9patch_png.sh
+++ fix_9patch_png.sh
@@ -16,19 +16,12 @@
 #   3.re-zip taget apk.
 # $1: the original apk name
 # $2: the original apk dir
-# $3: the target dir
-# $4: just use to decide wether the apk is the stock app.
+# $3: the out dir
+# $4: filter dir
+# $5: filter dir
 
 APK_FILE=$1.apk
-
-#STOCK APP
-if [ -z $4 ];then
-	ORIGINAL_APK=stockrom/system/app/$APK_FILE
-#MIUI APP
-else
-	ORIGINAL_APK=$2/$APK_FILE
-fi
-
+ORIGINAL_APK=$2/$APK_FILE
 REPLACE_APK=$3/$APK_FILE
 
 TMP_ORIGINAL_FILE=$3/$1-original.apk
@@ -37,15 +30,30 @@ TMP_TARGET_FILE=$3/$1-target.apk
 TMP_ORIGINAL_DIR=$3/$1-original
 TMP_TARGET_DIR=$3/$1-target
 
+HAS_FILTER1=false
+HAS_FILTER2=false
+if [ $# -gt 3 ];then
+	HAS_FILTER1=true
+fi
+if [ $# -gt 4 ];then
+	HAS_FILTER2=true
+fi
+
 cp -r $ORIGINAL_APK $TMP_ORIGINAL_FILE
 mv  $REPLACE_APK $TMP_TARGET_FILE
-unzip $TMP_ORIGINAL_FILE -d $TMP_ORIGINAL_DIR
-unzip $TMP_TARGET_FILE -d $TMP_TARGET_DIR
+unzip -o $TMP_ORIGINAL_FILE -d $TMP_ORIGINAL_DIR > /dev/null
+unzip -o $TMP_TARGET_FILE -d $TMP_TARGET_DIR > /dev/null
 for file in `find $TMP_ORIGINAL_DIR -name *.9.png`; do
 	targetfile=`echo $file | sed -e "s/-original/-target/"`
+	overlay=`echo $file | sed -e "s/$3\/$1-original\/res\///"`
+	if [ "$HAS_FILTER1" == "true" -a -f $4/$overlay ];then
+		continue
+	elif [ "$HAS_FILTER2" == "true" -a -f $5/$overlay ];then
+		continue
+	fi
 	cp $file $targetfile
 done
 cd $TMP_TARGET_DIR
 #only store all files, not compress files
 #as raw resource can't be compressed.
-zip -r0 ../$APK_FILE ./
+zip -r0 ../$APK_FILE ./ > /dev/null
